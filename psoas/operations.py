ca22cc83f19101dee7455cc720ce18b381ef5640
Marvin Meyer <marmey@mail.upb.de>
2021-08-02 13:13:58 +0200

    Added calc_max_iter to operations.

--- psoas/operations.py
+++ psoas/operations.py
@@ -20,6 +20,9 @@ def random_hypersphere_draw(r, dim):
     u *= r[:,None]
     return u[:,:dim]
 
+def calc_max_iter(max_f_eval, n_particles):
+    max_iter = np.floor((max_f_eval - n_particles)/n_particles)
+    return max_iter.astype('int')
 
 class counting_function():
     def __init__(self, function):
